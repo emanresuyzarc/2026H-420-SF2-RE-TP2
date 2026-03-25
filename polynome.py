e8349b2798d99d19bb2bac112339b66759ff95df
emanresuyzarc <enzo.sanchez.valero@gmail.com>
2026-03-25 10:09:54 -0400

    Modification classe Polynome

--- polynome.py
+++ polynome.py
@@ -5,4 +5,39 @@ class Polynome(Expression):
     """Polynome represente par une liste de coefficients [a0, a1, a2, ...]."""
 
     # Votre code ici (remplacer le "pass" par votre implementation)
-    pass
+    def __init__(self, liste_coef):
+        self.liste_coef = liste_coef
+
+
+    def evaluer(self, x: float) -> float:
+        """Retourne la valeur numerique de l'expression pour x."""
+
+
+    def deriver(self) -> "Expression":
+        expr_derivee = []
+        i = 0
+        for element in self.liste_coef:
+            expr_derivee.append(element*i)
+            i += 1
+
+        return Polynome(expr_derivee)
+
+
+    def __str__(self) -> str:
+        expr_lisible = []
+        i = 0
+
+        for element in self.liste_coef:
+            if i == 0:
+                terme = element
+
+            elif i == 1:
+                terme = f"{element}x"
+
+            else:
+                terme = f"{element}x^{i}"
+
+        expr_lisible.append(terme)
+
+        return " + ".join(expr_lisible)
+                
